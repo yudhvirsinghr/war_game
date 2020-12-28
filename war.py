85fb4ae49239011c45376a60a8ecc3f73d4db823
Yudhvir Singh <singh@Yudhvirs-MacBook-Air.local>
2020-12-27 16:04:18 -0800

    game logic

--- war.py
+++ war.py
@@ -1,13 +1,85 @@
 from src import Card, Deck, Player
 
-two_of_hearts = Card('Hearts', 'Two')
+# Assign two players to play the game
+player_one = Player('One')
+player_two = Player('Two')
 
+# make a deck of 52 cards and shuffle them
 new_deck = Deck()
 new_deck.shuffle()
 
-new_player = Player('Singh')
+# distribute the cards evenly to 2 players
+for card in range(52):
+    if card % 2 == 0:
+        player_one.add_cards(new_deck.deal_one())
+    else:
+        player_two.add_cards(new_deck.deal_one())
 
-my_card = new_deck.deal_one()
-new_player.add_cards([my_card,my_card])
 
-print(new_player)
+# WAR Game
+game_on = True
+
+round_num = 0
+
+while game_on:
+    round_num += 1
+    print(f'Round {round_num}')
+
+    # check if any player has no cards left to play
+    if len(player_one.cards) == 0:
+        print('Player One, out of cards ! Player Two Wins!')
+        game_on = False
+        break
+
+    elif len(player_two.cards) == 0:
+        print('Player Two, out of cards ! Player One Wins!')
+        game_on = False
+        break
+
+    # Start putting the cards out
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+    print(player_one_cards[-1])
+    print(player_two_cards[-1])
+
+    # war
+    war_on = True
+
+    while war_on:
+
+
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+
+            war_on = False
+
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+
+            war_on = False
+
+        else:
+            print('WAR!')
+
+            if len(player_one.cards) < 3:
+                print('Player One is out of cards to play war')
+                print('Player Two wins')
+                game_on = False
+                break
+
+            elif len(player_two.cards) < 3:
+                print('Player Two is out of cards to play war')
+                print('Player One wins')
+                game_on = False
+                break
+
+            else:
+                for num in range(3):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
